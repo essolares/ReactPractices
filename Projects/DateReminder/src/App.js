8904911e87c77a36c1a5970a0d8d1a5201c309d2
Edgar Salazar <essolares@gmail.com>
2021-01-04 19:07:34 -0600

    Project updates

--- Projects/DateReminder/src/App.js
+++ Projects/DateReminder/src/App.js
@@ -2,8 +2,13 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
+    const [people,setPeople] = useState(data);
 return <main>
-    
+    <section className="container">
+    <h3>{people.length} meetings today</h3>
+    <List people={people}/>
+    <button onClick={()=> setPeople([])}>Clear List</button>
+    </section>
 </main>;
 }
 
