220da3635db038833a2e917d36397a4613ddb884
Edgar Salazar <essolares@gmail.com>
2021-01-06 11:36:09 -0600

    Survey project
    Survey

---
+++ Projects/ReviewBox/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { FaGithubSquare } from 'react-icons/fa';
+import Review from './Review';
+
+function App() {
+  return <main>
+    <div className="container">
+        <div className="title">
+          <h2>Our Review</h2>
+          <div className="underline"></div>
+        </div>
+        <Review/>
+    </div>
+  </main>;
+}
+
+export default App;
