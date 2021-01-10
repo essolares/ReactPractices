58faf9353363904343211a8eda1ab7968edd7239
Edgar Salazar <essolares@gmail.com>
2021-01-10 08:17:52 -0600

    NavigationBar
    11- NAVIGATION BAR
    ———————————————————————————————
    USEREF HOOK TO UPDATE CSS AND EVAL CSS PROERTIES
    HIDE AND SHOW MENU FOR SCREEN SIZE
    DYNAMIC MANU AND SOCIAL

---
+++ Projects/NavigationBar/src/Navbar.js
@@ -0,0 +1,52 @@
+import React, { useState, useRef, useEffect } from 'react'
+import { FaBars, FaTwitter } from 'react-icons/fa'
+import { links, social } from './data'
+import logo from './logo.svg'
+
+const Navbar = () => {
+  const [showLinks,setShowLinks]=useState(false)
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(()=>{
+    const linkHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks){
+      linksContainerRef.current.style.height = `${linkHeight}px`
+    }else{
+      linksContainerRef.current.style.height = '0px'
+    }
+  },[showLinks])
+
+  return <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo"/>
+        <button className="nav-toggle" onClick={()=> setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </div>
+      <div className='links-container' ref={linksContainerRef}>
+        <ul className="links" ref={linksRef}>
+          {links.map((item)=>{
+            return (
+              <li key={item.id}>
+                <a href={item.url}>{item.text}</a>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((item)=>{
+            return (
+              <li key={item.id}>
+                <a href={item.url}>{item.icon}</a>
+              </li>
+            )
+          })}
+      </ul>
+    </div>
+  </nav>
+}
+
+export default Navbar
